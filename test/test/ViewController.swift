91ad298233b21b83c3c2d3d241ef2e7581ea542d
hovhannes <aksel@fifth-llc.com>
2022-08-30 15:18:54 +0400

    commit message

---
+++ test/test/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Aksel Avetisyan on 30.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
